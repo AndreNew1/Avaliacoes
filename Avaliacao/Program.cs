b3c067aada0187cbc43bd5edc49bde038159eaaf
AndreNew1 <elias.victor1997@gmail.com>
2019-07-19 15:28:06 -0300

    19/07/2019

---
+++ Avaliacao/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Globalization;
+namespace Avaliacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ANome = new string[5];
+            double[] Notas = new double[5];
+            int i = 0,aprovado=0,reprovado=0;
+            double sum=0;
+
+            try
+            {
+                //Recepção dos dados
+                do
+                {
+                    Console.WriteLine("Digite o Nome do Aluno");
+                    string Nome = Console.ReadLine();
+                    try
+                    {
+                        Console.WriteLine("Digite a Nota do Aluno");
+                        double Nota = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        //Tratamento de erro Notas
+                        while (Nota > 10 || Nota < 0)
+                        {
+                            Console.WriteLine("Nota Invalida");
+                            Console.WriteLine("Digite a Nota do Aluno");
+                            Nota = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        }
+                        ANome[i] = Nome;
+                        Notas[i] = Nota;
+                        i++;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("ERROR");
+                        Console.ReadLine();
+                    }
+                    Console.Clear();
+                } while (i < 5);
+
+                //Imprimi Alunos Aprovados
+                Console.WriteLine("Alunos Aprovados :");
+                for (i = 0; i < 5; i++)
+                {
+                    sum += Notas[i];
+                    if (Notas[i] >= 5)
+                    {
+                        aprovado++;
+                        Console.WriteLine("Nome do Aluno: {0} Nota: {1} Estado: Aprovado", ANome[i], Notas[i]);
+                    }
+                    else
+                    {
+                        reprovado++;
+                        Console.WriteLine("Nome do Aluno: {0} Nota: {1} Estado: Reprovado", ANome[i], Notas[i]);
+                    }
+
+                }
+                //Media Sala
+                double MediaSala = sum / 5;
+                Console.WriteLine();
+                Console.WriteLine("Media da Sala: " + MediaSala);
+
+                //Porcentagem dos Alunos
+                reprovado = reprovado * 20;
+                aprovado = aprovado * 20;
+                Console.WriteLine();
+                Console.WriteLine("Alunos Aprovados: " + aprovado + "%");
+                Console.WriteLine("Alunos Reprovados: " + reprovado + "%");
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ Avaliacao3/Avaliacao3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Avaliacao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Par = new List<int>();
+            List<int> Impar = new List<int>();
+
+            for(int i = 0; i <= 100; i++)
+            {
+                if (i % 2 == 0) { Par.Add(i); } else { Impar.Add(i); }
+            }
+            Print(Par);
+            Print(Impar);
+            Console.Write("7° Numero:"+Par[6]+"\n");
+            Console.WriteLine("20° Numero:"+Impar[19]);
+            AcresDesc(Par);
+            AcresDesc(Impar);
+            Print(Par);
+            Print(Impar);
+        }
+        static void Print(List<int>s)
+        {
+            foreach (int a in s)
+            {if (a.Equals(100) || a.Equals(99)||a.Equals(101))
+                    Console.Write(a + ".");
+                else Console.Write(a + "-");
+            }
+            Console.WriteLine("\n");
+        }
+        static void AcresDesc(List<int> s)
+        {
+            for(int i = 0; i <= s.Count-1; i++)
+            {
+                if (s[i] % 2 == 0)
+                    s[i] += 1;
+                else s[i] -= 1;
+            }
+            if (s[0] % 2 == 0) { s.Add(100); }
+            s.Remove(101);
+        }
+    }
+}
